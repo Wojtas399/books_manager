2c35efbd89873818676df7381ee1741ac2bbbbb7
Wojtas399 <wojtekp26@gmail.com>
2022-08-25 09:24:06 +0200

    Refactored slide route animation classes.

--- lib/config/animations/slide_left_route_animation.dart
+++ lib/config/animations/slide_left_route_animation.dart
@@ -1,24 +1,9 @@
-import 'package:flutter/widgets.dart';
+import 'slide_route_animation.dart';
 
-class SlideLeftRouteAnimation<T> extends PageRouteBuilder<T> {
-  final Widget page;
-
-  SlideLeftRouteAnimation({required this.page})
-      : super(
-    pageBuilder: (_, animation, secondaryAnimation) => page,
-    transitionsBuilder: (_, animation, secondaryAnimation, child) {
-      const begin = Offset(1.0, 0.0);
-      const end = Offset.zero;
-      const curve = Curves.fastOutSlowIn;
-
-      var tween = Tween(begin: begin, end: end).chain(
-        CurveTween(curve: curve),
-      );
-
-      return SlideTransition(
-        position: animation.drive(tween),
-        child: child,
-      );
-    },
-  );
+class SlideLeftRouteAnimation extends SlideRouteAnimation {
+  SlideLeftRouteAnimation({
+    required super.page,
+    super.dx = 1,
+    super.dy = 0,
+  });
 }
--- lib/config/animations/slide_right_route_animation.dart
+++ lib/config/animations/slide_right_route_animation.dart
@@ -1,24 +1,9 @@
-import 'package:flutter/widgets.dart';
+import 'slide_route_animation.dart';
 
-class SlideRightRouteAnimation<T> extends PageRouteBuilder<T> {
-  final Widget page;
-
-  SlideRightRouteAnimation({required this.page})
-      : super(
-          pageBuilder: (_, animation, secondaryAnimation) => page,
-          transitionsBuilder: (_, animation, secondaryAnimation, child) {
-            const begin = Offset(-1.0, 0.0);
-            const end = Offset.zero;
-            const curve = Curves.fastOutSlowIn;
-
-            var tween = Tween(begin: begin, end: end).chain(
-              CurveTween(curve: curve),
-            );
-
-            return SlideTransition(
-              position: animation.drive(tween),
-              child: child,
-            );
-          },
-        );
+class SlideRightRouteAnimation extends SlideRouteAnimation {
+  SlideRightRouteAnimation({
+    required super.page,
+    super.dx = -1,
+    super.dy = 0,
+  });
 }

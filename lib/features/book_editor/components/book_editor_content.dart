3e4e7f3fdb38275e9c283776cb990f735bf94229
Wojtas399 <wojtekp26@gmail.com>
2022-09-14 10:02:12 +0200

    Book editor title and author.

--- lib/features/book_editor/components/book_editor_content.dart
+++ lib/features/book_editor/components/book_editor_content.dart
@@ -1,6 +1,7 @@
 import 'package:app/components/custom_scaffold.dart';
 import 'package:app/components/on_tap_focus_lose_area_component.dart';
 import 'package:app/features/book_editor/components/book_editor_image.dart';
+import 'package:app/features/book_editor/components/book_editor_title_author.dart';
 import 'package:flutter/widgets.dart';
 
 class BookEditorContent extends StatelessWidget {
@@ -16,8 +17,10 @@ class BookEditorContent extends StatelessWidget {
           child: Padding(
             padding: const EdgeInsets.all(24),
             child: Column(
-              children: const <Widget>[
+              children: const [
                 BookEditorImage(),
+                SizedBox(height: 24),
+                BookEditorTitleAuthor(),
               ],
             ),
           ),

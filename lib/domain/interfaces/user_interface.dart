5491762a6f04d7798b937358d66e1a82c641bdc7
Wojtas399 <wojtekp26@gmail.com>
2022-09-18 08:03:16 +0200

    User interface.

---
+++ lib/domain/interfaces/user_interface.dart
@@ -0,0 +1,21 @@
+import 'package:app/domain/entities/user.dart';
+
+abstract class UserInterface {
+  Future<void> refreshUser({required String userId});
+
+  Stream<User> getUser({required String userId});
+
+  Future<void> loadUser({required String userId});
+
+  Future<void> addUser({required User user});
+
+  Future<void> updateUser({
+    required String userId,
+    bool? isDarkModeOn,
+    bool? isDarkModeCompatibilityWithSystemOn,
+  });
+
+  Future<void> deleteUser({required String userId});
+
+  void reset();
+}

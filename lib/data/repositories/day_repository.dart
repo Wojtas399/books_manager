95b4888f007f17e4758b1b6f62137fd368f41d46
Wojtas399 <wojtekp26@gmail.com>
2022-09-26 21:46:52 +0200

    New methods in day interface.

--- lib/data/repositories/day_repository.dart
+++ lib/data/repositories/day_repository.dart
@@ -25,11 +25,29 @@ class DayRepository implements DayInterface {
   }
 
   @override
-  Future<void> addReadPagesForUser({
+  Future<void> initializeForUser({required String userId}) async {
+    throw UnimplementedError();
+    //TODO: implement this method
+  }
+
+  @override
+  Future<void> loadUserDays({required String userId}) async {
+    throw UnimplementedError();
+    //TODO: implement this method
+  }
+
+  @override
+  Future<void> addUserReadBook({
     required String userId,
     required String bookId,
     required int readPagesAmount,
   }) {
+    throw UnimplementedError();
+    //TODO: implement this method
+  }
+
+  @override
+  void reset() {
     throw UnimplementedError();
     //TODO: implement this method
   }
--- lib/domain/interfaces/day_interface.dart
+++ lib/domain/interfaces/day_interface.dart
@@ -3,9 +3,15 @@ import 'package:app/domain/entities/day.dart';
 abstract class DayInterface {
   Stream<List<Day>> getUserDays({required String userId});
 
-  Future<void> addReadPagesForUser({
+  Future<void> initializeForUser({required String userId});
+
+  Future<void> loadUserDays({required String userId});
+
+  Future<void> addUserReadBook({
     required String userId,
     required String bookId,
     required int readPagesAmount,
   });
+
+  void reset();
 }

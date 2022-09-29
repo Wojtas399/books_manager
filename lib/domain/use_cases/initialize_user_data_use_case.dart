ca2bbb594150d78f615aefba28396e1da39a48c9
Wojtas399 <wojtekp26@gmail.com>
2022-09-29 21:13:48 +0200

    Added call of method responsible for initializing user days to initialize user data use case.

--- lib/domain/use_cases/initialize_user_data_use_case.dart
+++ lib/domain/use_cases/initialize_user_data_use_case.dart
@@ -1,21 +1,26 @@
 import 'package:app/domain/interfaces/book_interface.dart';
+import 'package:app/domain/interfaces/day_interface.dart';
 import 'package:app/domain/interfaces/user_interface.dart';
 
 class InitializeUserDataUseCase {
   late final UserInterface _userInterface;
   late final BookInterface _bookInterface;
+  late final DayInterface _dayInterface;
 
   InitializeUserDataUseCase({
     required UserInterface userInterface,
     required BookInterface bookInterface,
+    required DayInterface dayInterface,
   }) {
     _userInterface = userInterface;
     _bookInterface = bookInterface;
+    _dayInterface = dayInterface;
   }
 
   Future<void> execute({required String userId}) async {
     await _userInterface.refreshUser(userId: userId);
     await _bookInterface.refreshUserBooks(userId: userId);
+    await _dayInterface.initializeForUser(userId: userId);
     await _userInterface.loadUser(userId: userId);
   }
 }
--- test/domain/use_cases/initialize_user_data_use_case_test.dart
+++ test/domain/use_cases/initialize_user_data_use_case_test.dart
@@ -3,22 +3,26 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
 
 import '../../mocks/interfaces/mock_book_interface.dart';
+import '../../mocks/interfaces/mock_day_interface.dart';
 import '../../mocks/interfaces/mock_user_interface.dart';
 
 void main() {
   final userInterface = MockUserInterface();
   final bookInterface = MockBookInterface();
+  final dayInterface = MockDayInterface();
   final useCase = InitializeUserDataUseCase(
     userInterface: userInterface,
     bookInterface: bookInterface,
+    dayInterface: dayInterface,
   );
 
   test(
-    'should call methods responsible for refreshing user and his books and should call method responsible for loading user data',
+    'should call methods responsible for refreshing user and his books, should call method responsible for initializing user days and should call method responsible for loading user data',
     () async {
       const String userId = 'u1';
       userInterface.mockRefreshUser();
       bookInterface.mockRefreshUserBooks();
+      dayInterface.mockInitializeForUser();
       userInterface.mockLoadUser();
 
       await useCase.execute(userId: userId);
@@ -29,6 +33,9 @@ void main() {
       verify(
         () => bookInterface.refreshUserBooks(userId: userId),
       ).called(1);
+      verify(
+        () => dayInterface.initializeForUser(userId: userId),
+      );
       verify(
         () => userInterface.loadUser(userId: userId),
       ).called(1);

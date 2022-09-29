5bc69d8616a9d26f2c2ef15b0a9c8c7edf193f4e
Wojtas399 <wojtekp26@gmail.com>
2022-09-29 18:47:06 +0200

    Provider of day interface.

--- lib/providers/data_provider.dart
+++ lib/providers/data_provider.dart
@@ -1,12 +1,15 @@
 import 'package:app/data/data_sources/local_db/auth_local_db_service.dart';
 import 'package:app/data/data_sources/local_db/book_local_db_service.dart';
+import 'package:app/data/data_sources/local_db/day_local_db_service.dart';
 import 'package:app/data/data_sources/local_db/local_storage_service.dart';
 import 'package:app/data/data_sources/local_db/shared_preferences_service.dart';
 import 'package:app/data/data_sources/local_db/sqlite/services/sqlite_book_service.dart';
+import 'package:app/data/data_sources/local_db/sqlite/services/sqlite_read_book_service.dart';
 import 'package:app/data/data_sources/local_db/sqlite/services/sqlite_user_service.dart';
 import 'package:app/data/data_sources/local_db/user_local_db_service.dart';
 import 'package:app/data/data_sources/remote_db/auth_remote_db_service.dart';
 import 'package:app/data/data_sources/remote_db/book_remote_db_service.dart';
+import 'package:app/data/data_sources/remote_db/day_remote_db_service.dart';
 import 'package:app/data/data_sources/remote_db/firebase/services/firebase_auth_service.dart';
 import 'package:app/data/data_sources/remote_db/firebase/services/firebase_firestore_book_service.dart';
 import 'package:app/data/data_sources/remote_db/firebase/services/firebase_firestore_user_service.dart';
@@ -15,11 +18,14 @@ import 'package:app/data/data_sources/remote_db/user_remote_db_service.dart';
 import 'package:app/data/id_generator.dart';
 import 'package:app/data/repositories/auth_repository.dart';
 import 'package:app/data/repositories/book_repository.dart';
+import 'package:app/data/repositories/day_repository.dart';
 import 'package:app/data/repositories/user_repository.dart';
 import 'package:app/data/synchronizers/book_synchronizer.dart';
+import 'package:app/data/synchronizers/day_synchronizer.dart';
 import 'package:app/data/synchronizers/user_synchronizer.dart';
 import 'package:app/domain/interfaces/auth_interface.dart';
 import 'package:app/domain/interfaces/book_interface.dart';
+import 'package:app/domain/interfaces/day_interface.dart';
 import 'package:app/domain/interfaces/user_interface.dart';
 import 'package:app/providers/device_provider.dart';
 
@@ -60,6 +66,18 @@ class DataProvider {
     );
   }
 
+  static DayInterface provideDayInterface() {
+    return DayRepository(
+      daySynchronizer: DaySynchronizer(
+        dayLocalDbService: _provideDayLocalDbService(),
+        dayRemoteDbService: _provideDayRemoteDbService(),
+      ),
+      dayLocalDbService: _provideDayLocalDbService(),
+      dayRemoteDbService: _provideDayRemoteDbService(),
+      device: DeviceProvider.provide(),
+    );
+  }
+
   static UserLocalDbService _provideUserLocalDbService() {
     return UserLocalDbService(
       sqliteUserService: SqliteUserService(),
@@ -83,6 +101,18 @@ class DataProvider {
     return BookRemoteDbService(
       firebaseFirestoreBookService: FirebaseFirestoreBookService(),
       firebaseStorageService: FirebaseStorageService(),
+    );
+  }
+
+  static DayLocalDbService _provideDayLocalDbService() {
+    return DayLocalDbService(
+      sqliteReadBookService: SqliteReadBookService(),
+    );
+  }
+
+  static DayRemoteDbService _provideDayRemoteDbService() {
+    return DayRemoteDbService(
+      firebaseFirestoreUserService: FirebaseFirestoreUserService(),
     );
   }
 }

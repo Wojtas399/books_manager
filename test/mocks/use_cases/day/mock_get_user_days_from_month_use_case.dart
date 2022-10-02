afbd4453f4f8020fc312ea7aec4e2340c22a7d1c
Wojtas399 <wojtekp26@gmail.com>
2022-10-02 19:31:02 +0200

    Mock of get user days from month use case.

---
+++ test/mocks/use_cases/day/mock_get_user_days_from_month_use_case.dart
@@ -0,0 +1,16 @@
+import 'package:app/domain/entities/day.dart';
+import 'package:app/domain/use_cases/day/get_user_days_from_month_use_case.dart';
+import 'package:mocktail/mocktail.dart';
+
+class MockGetUserDaysFromMonthUseCase extends Mock
+    implements GetUserDaysFromMonthUseCase {
+  void mock({required List<Day> userDaysFromMonth}) {
+    when(
+      () => execute(
+        userId: any(named: 'userId'),
+        month: any(named: 'month'),
+        year: any(named: 'year'),
+      ),
+    ).thenAnswer((_) => Stream.value(userDaysFromMonth));
+  }
+}

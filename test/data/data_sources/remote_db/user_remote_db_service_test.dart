71166b9bd5ecad7efa428a5737350bab960a7d15
Wojtas399 <wojtekp26@gmail.com>
2022-10-17 12:11:57 +0200

    Moved folders with mocks of local db services, remote db services and synchronizers to data folder.

--- test/data/data_sources/remote_db/user_remote_db_service_test.dart
+++ test/data/data_sources/remote_db/user_remote_db_service_test.dart
@@ -4,7 +4,7 @@ import 'package:app/domain/entities/user.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
 
-import '../../../mocks/remote_db/firebase/mock_firebase_firestore_user_service.dart';
+import '../../../mocks/data/remote_db/firebase/mock_firebase_firestore_user_service.dart';
 
 void main() {
   final firebaseFirestoreUserService = MockFirebaseFirestoreUserService();
--- test/data/repositories/user_repository_test.dart
+++ test/data/repositories/user_repository_test.dart
@@ -4,10 +4,10 @@ import 'package:app/domain/entities/user.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
 
-import '../../mocks/local_db/mock_user_local_db_service.dart';
+import '../../mocks/data/local_db/mock_user_local_db_service.dart';
+import '../../mocks/data/remote_db/mock_user_remote_db_service.dart';
+import '../../mocks/data/synchronizers/mock_user_synchronizer.dart';
 import '../../mocks/mock_device.dart';
-import '../../mocks/remote_db/mock_user_remote_db_service.dart';
-import '../../mocks/synchronizers/mock_user_synchronizer.dart';
 
 void main() {
   final userSynchronizer = MockUserSynchronizer();

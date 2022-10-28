d0f73d8a94382397f5a797ed260e047affaa372b
Wojtas399 <wojtekp26@gmail.com>
2022-10-28 20:52:45 +0200

    Adjusted use case responsible for updating current page number after reading to be consistent with new book interface.

--- lib/domain/use_cases/book/update_current_page_number_after_reading_use_case.dart
+++ lib/domain/use_cases/book/update_current_page_number_after_reading_use_case.dart
@@ -1,5 +1,9 @@
+import 'package:app/config/errors.dart';
+import 'package:app/domain/entities/book.dart';
+import 'package:app/domain/entities/read_book.dart';
 import 'package:app/domain/interfaces/book_interface.dart';
 import 'package:app/domain/use_cases/day/add_new_read_book_to_user_days_use_case.dart';
+import 'package:app/models/error.dart';
 
 class UpdateCurrentPageNumberAfterReadingUseCase {
   late final BookInterface _bookInterface;
@@ -14,48 +18,51 @@ class UpdateCurrentPageNumberAfterReadingUseCase {
   }
 
   Future<void> execute({
-    required String userId,
     required String bookId,
+    required String userId,
     required int newCurrentPageNumber,
   }) async {
-    // final int readPagesAmount =
-    //     await _countReadPagesAmount(bookId, newCurrentPageNumber);
-    // final ReadBook readBook = ReadBook(
-    //   bookId: bookId,
-    //   readPagesAmount: readPagesAmount,
-    // );
-    // await _bookInterface.updateBookData(
-    //   bookId: bookId,
-    //   readPagesAmount: newCurrentPageNumber,
-    // );
-    // await _addNewReadBookToUserDaysUseCase.execute(
-    //   userId: userId,
-    //   readBook: readBook,
-    // );
+    final int readPagesAmount =
+        await _countReadPagesAmount(bookId, userId, newCurrentPageNumber);
+    final ReadBook readBook = ReadBook(
+      bookId: bookId,
+      readPagesAmount: readPagesAmount,
+    );
+    await _bookInterface.updateBook(
+      bookId: bookId,
+      userId: userId,
+      readPagesAmount: newCurrentPageNumber,
+    );
+    await _addNewReadBookToUserDaysUseCase.execute(
+      userId: userId,
+      readBook: readBook,
+    );
   }
 
-  // Future<int> _countReadPagesAmount(
-  //   String bookId,
-  //   int newCurrentPageNumber,
-  // ) async {
-  //   final Book book = await _loadBook(bookId);
-  //   if (newCurrentPageNumber > book.allPagesAmount) {
-  //     throw const BookError(
-  //       code: BookErrorCode.newCurrentPageIsTooHigh,
-  //     );
-  //   } else if (newCurrentPageNumber <= book.readPagesAmount) {
-  //     throw const BookError(
-  //       code: BookErrorCode.newCurrentPageIsLowerThanReadPagesAmount,
-  //     );
-  //   }
-  //   return newCurrentPageNumber - book.readPagesAmount;
-  // }
-  //
-  // Future<Book> _loadBook(String bookId) async {
-  //   final Book? book = await _bookInterface.getBookById(bookId: bookId).first;
-  //   if (book == null) {
-  //     throw '(UpdateCurrentPageNumberAfterReadingUseCase) Cannot load book';
-  //   }
-  //   return book;
-  // }
+  Future<int> _countReadPagesAmount(
+    String bookId,
+    String userId,
+    int newCurrentPageNumber,
+  ) async {
+    final Book book = await _loadBook(bookId, userId);
+    if (newCurrentPageNumber > book.allPagesAmount) {
+      throw const BookError(
+        code: BookErrorCode.newCurrentPageIsTooHigh,
+      );
+    } else if (newCurrentPageNumber <= book.readPagesAmount) {
+      throw const BookError(
+        code: BookErrorCode.newCurrentPageIsLowerThanReadPagesAmount,
+      );
+    }
+    return newCurrentPageNumber - book.readPagesAmount;
+  }
+
+  Future<Book> _loadBook(String bookId, String userId) async {
+    final Book? book =
+        await _bookInterface.getBook(bookId: bookId, userId: userId).first;
+    if (book == null) {
+      throw '(UpdateCurrentPageNumberAfterReadingUseCase) Cannot load book';
+    }
+    return book;
+  }
 }
--- test/domain/use_cases/book/update_current_page_number_after_reading_use_case_test.dart
+++ test/domain/use_cases/book/update_current_page_number_after_reading_use_case_test.dart
@@ -1,5 +1,8 @@
+import 'package:app/config/errors.dart';
 import 'package:app/domain/entities/book.dart';
+import 'package:app/domain/entities/read_book.dart';
 import 'package:app/domain/use_cases/book/update_current_page_number_after_reading_use_case.dart';
+import 'package:app/models/error.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
 
@@ -10,8 +13,8 @@ void main() {
   final bookInterface = MockBookInterface();
   final addNewReadBookToUserDaysUseCase = MockAddNewReadBookToUserDaysUseCase();
   late UpdateCurrentPageNumberAfterReadingUseCase useCase;
-  const String userId = 'u1';
   const String bookId = 'b1';
+  const String userId = 'u1';
   final Book book = createBook(
     id: bookId,
     userId: userId,
@@ -21,8 +24,8 @@ void main() {
 
   Future<void> useCaseCall(int newCurrentPageNumber) async {
     await useCase.execute(
-      userId: userId,
       bookId: bookId,
+      userId: userId,
       newCurrentPageNumber: newCurrentPageNumber,
     );
   }
@@ -32,7 +35,7 @@ void main() {
       bookInterface: bookInterface,
       addNewReadBookToUserDaysUseCase: addNewReadBookToUserDaysUseCase,
     );
-    // bookInterface.mockGetBookById(book: book);
+    bookInterface.mockGetBook(book: book);
   });
 
   tearDown(() {
@@ -40,73 +43,74 @@ void main() {
     reset(addNewReadBookToUserDaysUseCase);
   });
 
-  // test(
-  //   'should throw book error if new current page is higher than all pages amount from the book',
-  //   () async {
-  //     final int newCurrentPageNumber = book.allPagesAmount + 1;
-  //     const BookError expectedBookError = BookError(
-  //       code: BookErrorCode.newCurrentPageIsTooHigh,
-  //     );
-  //     Object? error;
-  //
-  //     try {
-  //       await useCaseCall(newCurrentPageNumber);
-  //     } catch (useCaseError) {
-  //       error = useCaseError;
-  //     }
-  //
-  //     expect(error, expectedBookError);
-  //   },
-  // );
-  //
-  // test(
-  //   'should throw book error if new current page number is lower or equal to read pages amount from the book',
-  //   () async {
-  //     final int newCurrentPageNumber = book.readPagesAmount;
-  //     const BookError expectedBookError = BookError(
-  //       code: BookErrorCode.newCurrentPageIsLowerThanReadPagesAmount,
-  //     );
-  //     Object? error;
-  //
-  //     try {
-  //       await useCaseCall(newCurrentPageNumber);
-  //     } catch (useCaseError) {
-  //       error = useCaseError;
-  //     }
-  //
-  //     expect(error, expectedBookError);
-  //   },
-  // );
-  //
-  // test(
-  //   'should update book with new read pages amount set to given new current page number and should add read pages amount to user days',
-  //   () async {
-  //     const int newCurrentPageNumber = 120;
-  //     final ReadBook readBook = createReadBook(
-  //       bookId: bookId,
-  //       readPagesAmount: newCurrentPageNumber - book.readPagesAmount,
-  //     );
-  //     bookInterface.mockUpdateBookData();
-  //     addNewReadBookToUserDaysUseCase.mock();
-  //
-  //     await useCase.execute(
-  //       bookId: bookId,
-  //       userId: userId,
-  //       newCurrentPageNumber: newCurrentPageNumber,
-  //     );
-  //
-  //     verify(
-  //       () => bookInterface.updateBookData(
-  //         bookId: bookId,
-  //         readPagesAmount: newCurrentPageNumber,
-  //       ),
-  //     ).called(1);
-  //     verify(
-  //       () => addNewReadBookToUserDaysUseCase.execute(
-  //         userId: userId,
-  //         readBook: readBook,
-  //       ),
-  //     ).called(1);
-  //   },
-  // );
+  test(
+    'should throw book error if new current page is higher than all pages amount from the book',
+    () async {
+      final int newCurrentPageNumber = book.allPagesAmount + 1;
+      const BookError expectedBookError = BookError(
+        code: BookErrorCode.newCurrentPageIsTooHigh,
+      );
+      Object? error;
+
+      try {
+        await useCaseCall(newCurrentPageNumber);
+      } catch (useCaseError) {
+        error = useCaseError;
+      }
+
+      expect(error, expectedBookError);
+    },
+  );
+
+  test(
+    'should throw book error if new current page number is lower or equal to read pages amount from the book',
+    () async {
+      final int newCurrentPageNumber = book.readPagesAmount;
+      const BookError expectedBookError = BookError(
+        code: BookErrorCode.newCurrentPageIsLowerThanReadPagesAmount,
+      );
+      Object? error;
+
+      try {
+        await useCaseCall(newCurrentPageNumber);
+      } catch (useCaseError) {
+        error = useCaseError;
+      }
+
+      expect(error, expectedBookError);
+    },
+  );
+
+  test(
+    'should update book with new read pages amount set to given new current page number and should add read pages amount to user days',
+    () async {
+      const int newCurrentPageNumber = 120;
+      final ReadBook readBook = createReadBook(
+        bookId: bookId,
+        readPagesAmount: newCurrentPageNumber - book.readPagesAmount,
+      );
+      bookInterface.mockUpdateBook();
+      addNewReadBookToUserDaysUseCase.mock();
+
+      await useCase.execute(
+        bookId: bookId,
+        userId: userId,
+        newCurrentPageNumber: newCurrentPageNumber,
+      );
+
+      verify(
+        () => bookInterface.updateBook(
+          bookId: bookId,
+          userId: userId,
+          readPagesAmount: newCurrentPageNumber,
+        ),
+      ).called(1);
+      verify(
+        () => addNewReadBookToUserDaysUseCase.execute(
+          userId: userId,
+          readBook: readBook,
+        ),
+      ).called(1);
+    },
+  );
 }

cf7c2cbad0e5ef0e4e2e60899ccdf2bb87e9a335
Wojtas399 <wojtekp26@gmail.com>
2022-11-13 17:31:59 +0100

    Corrected methods in extension of list of days.

--- lib/extensions/list_of_days_extensions.dart
+++ lib/extensions/list_of_days_extensions.dart
@@ -1,10 +1,16 @@
 import 'package:app/domain/entities/day.dart';
 import 'package:app/extensions/list_of_read_books_extensions.dart';
+import 'package:app/utils/date_utils.dart';
 
 extension ListOfDaysExtensions on List<Day> {
   bool doesNotContainDate(DateTime date) {
     final List<DateTime> dates = map((Day day) => day.date).toList();
-    return !dates.contains(date);
+    for (int i = 0; i < dates.length; i++) {
+      if (DateUtils.areDatesTheSame(date1: dates[i], date2: date)) {
+        return false;
+      }
+    }
+    return true;
   }
 
   List<Day> selectDaysContainingBook(String bookId) {
@@ -12,6 +18,11 @@ extension ListOfDaysExtensions on List<Day> {
   }
 
   Day selectDayByDate(DateTime date) {
-    return firstWhere((Day day) => day.date == date);
+    return firstWhere(
+      (Day day) => DateUtils.areDatesTheSame(
+        date1: day.date,
+        date2: date,
+      ),
+    );
   }
 }

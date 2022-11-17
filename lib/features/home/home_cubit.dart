459831a3d0315a7fa7b5a74efa25537c1d986f2e
Wojtas399 <wojtekp26@gmail.com>
2022-11-17 16:17:10 +0100

    Added new method to home cubit responsible for initializing logged user books.

--- lib/features/home/home_cubit.dart
+++ lib/features/home/home_cubit.dart
@@ -1,7 +1,26 @@
+import 'package:app/domain/use_cases/auth/get_logged_user_id_use_case.dart';
+import 'package:app/domain/use_cases/book/initialize_user_books_use_case.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class HomeCubit extends Cubit<int> {
-  HomeCubit() : super(0);
+  late final GetLoggedUserIdUseCase _getLoggedUserIdUseCase;
+  late final InitializeUserBooksUseCase _initializeUserBooksUseCase;
+
+  HomeCubit({
+    required GetLoggedUserIdUseCase getLoggedUserIdUseCase,
+    required InitializeUserBooksUseCase initializeUserBooksUseCase,
+  }) : super(0) {
+    _getLoggedUserIdUseCase = getLoggedUserIdUseCase;
+    _initializeUserBooksUseCase = initializeUserBooksUseCase;
+  }
+
+  Future<void> initialize() async {
+    final String? loggedUserId = await _getLoggedUserIdUseCase.execute().first;
+    if (loggedUserId == null) {
+      return;
+    }
+    _initializeUserBooksUseCase.execute(userId: loggedUserId);
+  }
 
   void changePage(int pageIndex) {
     emit(pageIndex);

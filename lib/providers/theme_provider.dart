b31b43586447eb77131cc90b6d8bdab3abc92a35
Wojtas399 <wojtekp26@gmail.com>
2022-11-27 16:44:36 +0100

    Added new method to theme provider responsible for checking if dark mode is on.

--- lib/providers/theme_provider.dart
+++ lib/providers/theme_provider.dart
@@ -4,6 +4,13 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 class ThemeProvider extends Cubit<ThemeMode> {
   ThemeProvider() : super(ThemeMode.light);
 
+  bool isDarkModeOn(BuildContext context) {
+    final ThemeMode themeMode = state;
+    final Brightness brightness = MediaQuery.of(context).platformBrightness;
+    return themeMode == ThemeMode.dark ||
+        (themeMode == ThemeMode.system && brightness == Brightness.dark);
+  }
+
   void turnOnLightTheme() {
     emit(ThemeMode.light);
   }
